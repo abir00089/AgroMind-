aa9706a9c75c8ca47a6e36bf7b99753ef0417bff
Abir Mondal <abirmondal8926@gmail.com>
2026-04-09 01:23:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ if "water_logs" not in st.session_state:
     st.session_state.water_logs = []
 
 # ----------------- BACKEND -----------------
-API_URL = "https://agromind-server.onrender.com/data"
+API_URL = "https://agromind-backend.vercel.app/api/data"
 
 def get_sensor_data():
     try:
